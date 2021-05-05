b40a39649e975c9f13e62c336cb423f47ddc42a4
unknown <pdullabh315@gmail.com>
2021-05-05 19:23:59 +0200

    resume basic updates

--- src/Components/Content/Resume/ResumeDetails/Education/Education.js
+++ src/Components/Content/Resume/ResumeDetails/Education/Education.js
@@ -8,24 +8,26 @@ const education = () => {
         <i className="fas fa-graduation-cap"></i>
       </span>
       <h5>Education</h5>
-      <h6>Bachelor Of Technology (Graduation) June 2017 - Pursuing</h6>
-      <p>Lovely Professional Universitry, Punjab</p>
-      <p>Currently, I am pursuing my B.Tech in Computer Science and Engineering(CSE) from LPU and I am in 3rd year till now I got 7.16 CGPA Overall.</p>
-      <h6>Intermediate (12th) From CBSE Board (Apr 2015 - Mar 2017) </h6>
-      <p>Saraswati Vidya Mandir, Sindri, Dhanbad, Jharkhand </p>
-      <p>In 12th standard I had chosen Science stream(PCM) and Passed with 71.6 %.</p>
-      <h6>Matriculation (10th) From CBSE Board (Apr 2014 - Mar 2015)</h6>
-      <p>Saraswati Vidya Mandir, Sindri, Dhanbad, Jharkhand </p>
-      <p>I had successfully Passed with a 8.8 CGPA</p>
+      <h6>University of Cape Town (Cape Town, South Africa):</h6>
+      <h6>January 2019 – April 2020</h6>
+      <p>-  Honours in B.Sc. Computer Science, GPA: 77</p>
+      <h6>February 2016 - November 2018</h6>
+      <p>-	B.Sc. Computer Science and Business Computing</p>
+      <h6>Fairbairn College (Cape Town, South Africa, February 2011 – November 2015)</h6>
 
       <span>
         <i className="fas fa-graduation-cap"></i>
       </span>
-      <h5>Work Expericene</h5>
-      <h6>Software Developer Intern (Form OCT 2020 to Present)</h6>
-      <p>TestUnity-A Crowdsourced Testing Platform, Bengaluru, India</p>
-
-      
+      <h5>Acedemic Projects</h5>
+      <h6>AutoStar: SEMI-AUTOMATED DATA WAREHOUSE CONSTRUCTION</h6>
+      <p>-	This was my Honours project; I worked in a team of 2 and I was responsible for semi-automatically creating a star-schema from differently structured source repositories.</p>
+      <p>-	Java was the core language used for this project. I used it to semi-automatically develop the star-schema from both SQL and MongoDB databases. I also defined the ETL process by scripting the entire data pipeline from the source repository to the warehouse containing the generated star-schema.</p>
+      <h6>Bid4Cows: an auctioning web application</h6>
+      <p>-	This was my Capstone project; our team built a dynamic web application that allows users to buy and sell their cattle in an auction-based environment. </p>
+      <p>-	I worked in a team of 3, I was responsible for the backend and the tools I used were Java, JavaServer Pages, JDBC and SQL.</p>
+      <h6>Survival: a 2D 1-Player game + Tetris</h6>
+      <p>-	I created my own top-down shooter game and Tetris from scratch using Java and LibGDX.</p>
+      <p>-	For the top-down shooter game I added in basic AI using A* pathfinding. </p>
     </Auxillary>
     
   );
--- src/Components/Content/Resume/ResumeDetails/WorkExperience/WorkExperience.js
+++ src/Components/Content/Resume/ResumeDetails/WorkExperience/WorkExperience.js
@@ -7,16 +7,23 @@ const expertise = () => {
       <span>
         <i className="fas fa-shield-alt"></i>
       </span>
-      <h5>Expertise</h5>
-      <h6>Full Stack Developer</h6>
-      <p>I can done with both backend and front-end development</p>
-      <h6>Front End Developer</h6>
-      <p>A web developer with passion for building scalable web applications 
-      having decent knowledge of JavaScript and React.Js and working 
-      experience with Node.Js, Express.Js and Mongodb </p>
-      <h6>Backend Developer</h6>
+      <h5>Work Experience</h5>
+      <h6>Data Scientist, Shoprite Holdings (Cape Town, SA: Jan. 01, 2021 – Present)</h6>
+      <p>-	I was responsible for developing a hybrid-recommendation-engine for the business.
+      The engine consisted of 6 different ALS models each trained on various levels.
+      Business stakeholders were happy with the results and felt that the solution offered additional value.  
+      The models were successfully put into production and consumed internally and across digital channels.
+      My role also included assisting with the configs to ensure the models were scalable in production.
+      </p>
+      <p>-	Another and more recent project of mine consisted of creating definitions for whether customers have churned or not from categories. 
+      Having thousands of categories with each of them having their own lifecycle, I decided to generate dynamic definitions for each category using clustering. 
+      The predictive aspect of this project involved developing a classification model to accurately predict the likelihood of a customer churning within a variable time frame from now. After demoing the results and achieving an accuracy of +-80%, clients were pleased, and the model was productionalized.
+      </p>      
+      <h6>Data Science Intern, Shoprite Holdings (Cape Town, SA: Feb. 03, 2020 – Dec. 31, 2020)</h6>
       <p>
-      With working experience with Node.Js, Express.Js and Mongodb , Postgres
+      -	My main project entailed forecasting the demand for articles at different branches. 
+      Other projects for the year involved deriving meaningful clusters of customers, stores, and categories based on their price sensitivity. 
+      I then presented the results of these models and its impact on the business to the respective clients and received good feedback.  
       </p>
     </Auxillary>
   );
