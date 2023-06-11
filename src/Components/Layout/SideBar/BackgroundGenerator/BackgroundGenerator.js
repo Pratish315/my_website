95f8aee439366f007b3db024a1f8a7f4366f6093
Pratish <pdullabh@shoprite.co.za>
2023-06-11 02:56:41 +0200

    ensuring entire sidebar is responsive

--- src/Components/Layout/SideBar/BackgroundGenerator/BackgroundGenerator.js
+++ src/Components/Layout/SideBar/BackgroundGenerator/BackgroundGenerator.js
@@ -8,7 +8,7 @@ const BackgroundGenerator = (props) => {
 
         <div className = {BackgroundGeneratorStyle.bg}>
             <p className = {BackgroundGeneratorStyle.backgroundGenText}>CHOOSE YOUR 2 FAVOURITE COLOURS BELOW!</p> 
-            <div>
+            <div className={BackgroundGeneratorStyle.inputSection}>
             <input className = {BackgroundGeneratorStyle.input_colour} type="color" value={props.color} onChange={e => props.setColor(e.target.value)} />
             <input className = {BackgroundGeneratorStyle.input_colour} type="color" value={props.color2} onChange={e => props.setColor2(e.target.value)} />
             </div>
